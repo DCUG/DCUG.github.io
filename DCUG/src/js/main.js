a0e60ad3af0d65f04af6bb62087ee9d6c5bc3037
AzureAD\JurgenForster <jurgen.forster@mavention.nl>
2018-11-14 20:26:00 +0100

    Initial setup and branding

---
+++ DCUG/src/js/main.js
@@ -0,0 +1,16 @@
+
+
+
+/*
+ * Integration of a simple cookie bar.
+ */
+import SimpleCookieConsent from 'simple-cookie-consent';
+
+new SimpleCookieConsent(
+  {
+    // policyText: 'Your cookie text goes here',
+    // barClassName: '',
+    // additionalLink: 'privacy.html',
+    // additionalLinkText: 'Privacy Policy'
+  });
+
